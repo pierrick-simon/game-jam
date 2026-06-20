4dca34593c791d378f46371564ae71552de04d77
Ariel <ariel.amriou@epitech.eu>
2026-06-20 19:29:15 +0200

    finished level end

--- scripts/star_anim.gd
+++ scripts/star_anim.gd
@@ -2,13 +2,25 @@ extends Node2D
 
 @onready var animated_sprite = $AnimatedSprite2D
 
-var finished: bool = false;
+var finished: bool = false
+var rad: float = 0.0
+var rad_incr: float = 20.0
+
+@export_subgroup("Settings")
+@export var color: Color
 
 func _process(delta: float) -> void:
+	if rad > 1500:
+		return
 	if finished:
 		animated_sprite.visible = false
+		rad += rad_incr
+		queue_redraw()  # Demande à Godot d'appeler _draw()
 		return
 	animated_sprite.play("heart")
 
 func _on_animated_sprite_2d_animation_looped() -> void:
-	queue_free()
+	finished = true
+
+func _draw() -> void:
+	draw_circle(Vector2.ZERO, rad, color)  # position locale, pas globale
